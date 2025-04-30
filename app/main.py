f3b7566af40ebb9c42212958ed16ce03a50b5ec1
ddpinto <numberx07@gmail.com>
2025-04-30 14:00:38 -0500

    Actualizacion con pandas a app

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 def run():
+    '''
     data = read_csv.read_csv('world_population.csv')
     
     countries = list(map(lambda x: x['Country/Territory'], data))
     percentage = list(map(lambda x: x['World Population Percentage'], data))
+    '''
+    df = pd.read_csv('world_population.csv')
+    df = df[df['Continent'] == 'Africa']
+
+    countries = df['Country/Territory'].values 
+    percentage = df['World Population Percentage'].values
     charts.generate_pie_chart(countries, percentage)
+
+    data = read_csv.read_csv('world_population.csv')
 
     country = input('Type Country => ')
 
